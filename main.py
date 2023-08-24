3e35b0036a6568a538b70e5283739ffb8c50ca85
Sonny Kong <whysonnykong@gmail.com>
2023-08-24 15:05:37 +0100

    modified to extract from a directory

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import pdfplumber
-import sys
 import json
 import os
 #extract text from a pdf and store each page as a list item
+#/Volumes/Extreme SSD/Quest Labs/project_gmail/project_gmail/generated_cvs
 
 def main():
-    file=input('input the pdf directory \n')
+    dir=input('input the pdf directory \n')
+    if not dir:
+        print('Nothing entered, converting all pdfs in the current directory into JSON')
+        dir='.'
+    all_pdfs=[(os.path.join(dir,file),file) for file in os.listdir(dir) if file[-3:]=='pdf']
+
     text=[]
-    pdf = pdfplumber.open(file)
-    for page in pdf.pages:
-        text.append(page.extract_text(layout=False))
 
-    filename = pdf.stream.name
-    data=json.dumps({'filename': filename, 'text':text})
+    for file in all_pdfs:
+        pdf = pdfplumber.open(file[0])
+        for page in pdf.pages:
+            text.append(page.extract_text(layout=False))
 
-    with open(f'pdf.json','wb') as f:
-        f.write(data.encode('utf-8'))
+        filename = file[1].replace('.pdf','')
 
-    print(f'Successfully extracted pdf and saved as a json file')
+        print(f'\n   ----- file name is  {filename}\n')
+
+        data=json.dumps({'filename': filename, 'text':text})
+
+        with open(f'{filename}.json','wb') as f:
+            f.write(data.encode('utf-8'))
+
+        print(f'Successfully extracted {filename}')
 
 
     return data
